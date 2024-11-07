b591c847e5f49960924101e5d8ee99fe3eaf4969
Jhessael Siancas <siancas.jhessael@gmail.com>
2024-11-06 23:37:42 -0400

    segunda prueba TDD de calculadoraCadena no funciona pero lo esperaba

--- src/CalculadoraCadena.spec.js
+++ src/CalculadoraCadena.spec.js
@@ -3,7 +3,11 @@ import calcularCadena from "./CalculadoraCadena";
 const calculadora = new calcularCadena();
 
 describe("Calculadora de cadenas", () => {
-  it("deberia calcular la suma de los caracteres", () => {
+  it("deberia calcular la suma de los caracteres 1,2,3", () => {
     expect(calculadora.calcularCadena("1,2,3")).toEqual(6);
   });
+  it("deberia calcular la suma de los caracteres 2,2,3", () => {
+    expect(calculadora.calcularCadena("1,2,3")).toEqual(7);
+  });
+  
 });
