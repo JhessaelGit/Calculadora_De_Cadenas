b1ef5fcdaf46acba4b8c46c828d96e45eb371c55
Jhessael Siancas <siancas.jhessael@gmail.com>
2024-11-06 23:57:18 -0400

    Tercera prueba TDD de CalculadoraCadenas, falla como se esperaba

--- src/CalculadoraCadena.spec.js
+++ src/CalculadoraCadena.spec.js
@@ -18,5 +18,10 @@ describe("Calculadora de cadenas", () => {
   it("deberia calcular la suma de los caracteres //[;] 6;7;4", () => {
     expect(calculadora.calcularCadena("[;] 6;7;4")).toEqual(17);
   });
-  
+  it("deberia calcular la suma de los caracteres //[;] 6,3-2;1", () => {
+    expect(calculadora.calcularCadena("//[;] 6,3-2;1")).toEqual(12);
+  });
+  it("deberia Ignorar los numeros mayores a 1000 y retornar 2", () => {
+    expect(calculadora.calcularCadena("2,1001")).toEqual(2);
+  });
 });
