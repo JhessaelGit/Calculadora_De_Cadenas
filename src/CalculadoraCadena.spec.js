d40611f28196072086b605ae7dde64cf9998f89c
Jhessael Siancas <siancas.jhessael@gmail.com>
2024-11-06 23:50:16 -0400

    Se Agregaron las pruebas de la descripcion con el -

--- src/CalculadoraCadena.spec.js
+++ src/CalculadoraCadena.spec.js
@@ -9,5 +9,11 @@ describe("Calculadora de cadenas", () => {
   it("deberia calcular la suma de los caracteres 2,2,3", () => {
     expect(calculadora.calcularCadena("2,2,3")).toEqual(7);
   });
+  it("deberia calcular la suma de los caracteres 1,2,4,8", () => {
+    expect(calculadora.calcularCadena("1,2,4,8")).toEqual(15);
+  });
+  it("deberia calcular la suma de los caracteres 1-2,3", () => {
+    expect(calculadora.calcularCadena("1-2,3")).toEqual(6);
+  });
   
 });
