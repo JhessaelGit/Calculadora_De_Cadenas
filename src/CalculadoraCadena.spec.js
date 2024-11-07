db6757d1f4a7b476e6a6095db91ca238c57c184b
Jhessael Siancas <siancas.jhessael@gmail.com>
2024-11-06 23:33:04 -0400

    primera prueba tdd fallando de calculadoraCadena

--- src/CalculadoraCadena.spec.js
+++ src/CalculadoraCadena.spec.js
@@ -2,3 +2,8 @@ import calcularCadena from "./CalculadoraCadena";
 
 const calculadora = new calcularCadena();
 
+describe("Calculadora de cadenas", () => {
+  it("deberia calcular la suma de los caracteres", () => {
+    expect(calculadora.calcularCadena("1,2,3")).toEqual(0);
+  });
+});
