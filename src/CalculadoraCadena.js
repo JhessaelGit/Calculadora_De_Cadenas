cf55f62cd4c99079a9567fe0e721a9056a1b1280
Jhessael Siancas <siancas.jhessael@gmail.com>
2024-11-07 01:07:48 -0400

    Tercera prueba TDD para el calculador cadenas, refactorizacion del codigo ya que fallaba ahora da todas las pruebas hasta ahora

--- src/CalculadoraCadena.js
+++ src/CalculadoraCadena.js
@@ -1,12 +1,33 @@
 class CalcularCadena {
+    optenerNumeros(cadena)
+    {
+        let numeros=[];
+        let numero=0;
+        for(let i=0;i<cadena.length;i++)
+        {
+            let caracter=cadena.charAt(i);
+            console.log("fuera del while ",numero)
+            while((!isNaN(caracter) && caracter !== " " && i<cadena.length))
+            {
+                numero=numero*10+Number(caracter);
+                i++;
+                caracter=cadena.charAt(i)
+                console.log("hola", numero, Number(caracter))
+            }
+            numeros.push(numero)
+            numero=0;
+        }
+        return numeros;
+    }
   calcularCadena(cadena) {
-    let suma = 0;
-    let tamanioCadena=cadena.length
-    for (let i = 0; i <= tamanioCadena; i++) {
-      let caracter = cadena.charAt(i);
-      if (!isNaN(caracter) && caracter !== " ") {
-        suma = suma + Number(caracter);
-      }
+    let suma=0;
+    let numeros=this.optenerNumeros(cadena);
+    for(let i=0;i<numeros.length;i++)
+    {
+        if(numeros[i]<=1000)
+        {
+            suma=suma+numeros[i]
+        }
     }
     return suma;
   }
